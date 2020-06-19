5b80616cd9eccd9388bbe07305392f15ccccd3f7
Arthur Chang <07570.arthur.chang@ICT888.NET>
2020-06-19 15:20:28 +0800

    Adding GameRepository interface.

---
+++ TennisGame/Repositories/IGameRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TennisGame.Models;
+
+namespace TennisGame.Repositories
+{
+    public interface IGameRepository
+    {
+        Task<IEnumerable<Game>> GetAll();
+
+        Task<IEnumerable<Game>> GetBy(Expression<Func<bool, Game>> @by);
+
+        Task<Game> Get(Expression<Func<bool, Game>> @by);
+
+        Task Add(Game game);
+
+        Task Update(Game game);
+
+        Task Remove(Game game);
+    }
+}
