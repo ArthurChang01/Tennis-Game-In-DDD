97830162e09f2b7a5c2321d639cd58730a968cc1
Arthur Chang <07570.arthur.chang@ICT888.NET>
2020-06-19 15:32:51 +0800

    1. Update .gitignore:
    => Add some rules to avoiding ncrunch config to commit.
    2. Adding a MongoRepository

---
+++ TennisGame.Persistent/MongoRepository.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+using TennisGame.Models;
+
+namespace TennisGame.Persistent
+{
+    public class MongoRepository
+    {
+        private IMongoCollection<Game> _collection;
+
+        public MongoRepository(string conString)
+        {
+            var client = new MongoClient(conString);
+            var db = client.GetDatabase("Games");
+            _collection = db.GetCollection<Game>("TennisGames");
+        }
+    }
+}
